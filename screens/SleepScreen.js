eb8a0e90a20d0a3438085608096980b408071eb2
Katharine Zhang <katharine177@outlook.com>
2021-03-06 21:35:35 -0800

    made a timer for the sleep screen to track sleep duration

--- screens/SleepScreen.js
+++ screens/SleepScreen.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { ImageBackground, StyleSheet, View, Image, Button, Text, Alert } from 'react-native';
+import { TouchableHighlight } from 'react-native-gesture-handler';
 
 function SleepScreen(props) {
     const [time, setTime] = useState(new Date());
-    const [timer, setTimer] = useState(new Date());
-    const [test, setTest] = useState("")
-    // const [test, setTest] = useState(props.test)
+    const [timer, setTimer] = useState({sec: 0, min: 0, hr: 0});
 
     useEffect(() => {
         let secTimer = setInterval( () => {
@@ -16,34 +15,54 @@ function SleepScreen(props) {
         return () => clearInterval(secTimer);
     }, []);
 
-    useEffect(() => {
-        let secTimer = setInterval( () => {
-          setTimer(new Date())
-        },1000)
+    const handleWakeUp = () => {
+        props.navigation.navigate("WelcomeScreen")
+    }
+
+    const startTimer = () => {
+        run();
+        setInterval(run, 1000)
+    }
+
+    var updatedSec = timer.sec;
+    var updatedMin = timer.min;
+    var updatedHr = timer.hr;
+
+    const run = () => {
+        if (updatedSec === 60) {
+            updatedMin++;
+            updatedSec = 0;
+        }
+        if (updatedMin === 60) {
+            updatedHr++;
+            updatedMin = 0;
+        }
+        updatedSec++;
+        return setTimer({sec: updatedSec, min: updatedMin, hr: updatedHr})
+    }
     
-        return () => clearInterval(secTimer);
-    }, []);
-
-    // useEffect(() => {
-    //     setTest(props.test)
-    // }, [props.test]);
-
     return (
         <ImageBackground 
             style={styles.background}
         >
             <Text style={styles.timer}>
-                {`${pad(timer.getHours(), 2)}:${pad(timer.getMinutes(), 2)}:${pad(timer.getSeconds(), 2)}`}
+                {`${pad(timer.hr, 2)}:${pad(timer.min, 2)}:${pad(timer.sec, 2)}`}
             </Text>
-            <Text style={{color: "white", fontSize: 30}}>{test}</Text>
             <Text style={styles.time}>{`${pad(time.getHours(), 2)}:${pad(time.getMinutes(), 2)}`}</Text>
             <Text style={styles.message}>goodnight</Text>
-            <Button title="wake up" onPress={() => Alert.alert("Wake up", "Are you sure", [
-                {text: "Yes", onPress: () => props.navigation.navigate("WelcomeScreen")},
+            <TouchableHighlight 
+            style={styles.button}
+            onPress={() => Alert.alert("Are you sure you want to wake up right", "Are you sure", [
+                {text: "Yes", onPress: handleWakeUp},
                 {text: "No", onPress: ()  => console.log("no pressed")}
-            ])}/>
+            ])}>
+                <Text style={styles.buttonText}>Wake up</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={styles.button} onPress={startTimer}>
+                <Text style={styles.buttonText}>I'm going to sleep</Text>
+            </TouchableHighlight>
+            {/* <Button title="I'm going to sleep" onPress={start}/> */}
             <Image style={styles.logo}  source={require("../assets/koi-trans.png")} />
-            {/* <View style={styles.login}/> */}
         </ImageBackground>
     );
 }
@@ -60,6 +79,21 @@ const styles = StyleSheet.create({
         backgroundColor: "#051C43",
         justifyContent: "flex-start",
         alignItems: "center"
+    },
+    button: {
+        marginRight:40,
+        marginLeft:40,
+        marginTop:10,
+        paddingTop:10,
+        paddingBottom:10,
+        backgroundColor:'grey',
+        borderRadius:10,
+    },
+    buttonText: {
+        color:'#fff',
+        textAlign:'center',
+        paddingLeft : 10,
+        paddingRight : 10
     },
     login:{
         backgroundColor: "green",
