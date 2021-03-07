e14a491c721a7a2a8a6c0b9ffa4706a753455bf5
Elizabeth <elizabethmcd4@gmail.com>
2021-03-07 01:30:17 -0800

    Added Time Asleep Calculator

---
+++ SleepTimeCalculator.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function removeMinuteAndSecondData(date) {
+    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+}
+
+function calculateDaysBetween(date1, date2) {
+    var days = (Date.parse(date2) - Date.parse(date1)) / (24*60*60*1000);
+    return Math.trunc(days);
+}
+
+function sleepTimeInHours(startDate, endDate) {    
+    var startHour = startDate.getHours();
+    var endHour = endDate.getHours();
+
+    var startDay = startDate.getDate();
+    var endDay = endDate.getDate();
+    
+    var startNoMins = removeMinuteAndSecondData(startDate);
+    var endNoMins = removeMinuteAndSecondData(endDate);
+    
+    var numDaysBetween = calculateDaysBetween(startNoMins, endNoMins);
+
+    if (numDaysBetween == 0) {
+        return (endHour - startHour);
+    } else {
+        return (24*(daysBetween-1) + (24 - startHour) + endHour);
+    }
+}
